f8d28fbe3be4580fdd829f9c3749d5342c216d48
Tonći Vatavuk <tonci@sistemi.hr>
2021-01-21 12:53:15 +0100

    create 3 separate nupkg's for win, llinux, osx runtimes fix nuget  warnings: NU5048; NU5128

--- Src/Package/release.cmd
+++ Src/Package/release.cmd
@@ -5,4 +5,6 @@ del /F/Q/S "..\Server\bin\Release\net5.0\publish" > NUL
 rmdir /Q/S "..\Server\bin\Release\net5.0\publish"
 dotnet publish ..\Server\ToSic.ImageFlow.Oqt.Server.csproj /p:Configuration=Release
 "..\..\..\oqtane.framework\oqtane.package\nuget.exe" pack ToSic.ImageFlow.Oqt.nuspec
+"..\..\..\oqtane.framework\oqtane.package\nuget.exe" pack ToSic.ImageFlow.Oqt-linux.nuspec
+"..\..\..\oqtane.framework\oqtane.package\nuget.exe" pack ToSic.ImageFlow.Oqt-osx.nuspec
 xcopy "*.nupkg" "..\..\InstallPackages" /Y
