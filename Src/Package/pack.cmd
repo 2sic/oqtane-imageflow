d794cc9d87265985f7cf522fcb7bc85ad8f11efc
Tonći Vatavuk <tonci@sistemi.hr>
2022-10-10 22:50:22 +0200

    nuspec update

--- Src/Package/pack.cmd
+++ Src/Package/pack.cmd
@@ -5,12 +5,12 @@ del /F/Q/S "..\Server\bin\Release\net6.0\publish" > NUL
 rmdir /Q/S "..\Server\bin\Release\net6.0\publish"
 dotnet publish ..\Server\ToSic.Imageflow.Oqtane.Server.csproj /p:Configuration=Release
 @REM By convention if you place the oqtane module root folder at the same level as the oqtane framework, then you will not have to modify any folder configuration.
-"..\..\..\oqtane.framework\oqtane.package\nuget.exe" pack ToSic.Imageflow.Oqtane.win.nuspec
+.nuget\nuget.exe pack ToSic.Imageflow.Oqtane.win.nuspec
 ren ToSic.Imageflow.Oqtane.????.????.????.nupkg ToSic.Imageflow.Oqtane.????.????.????-win.nupkg
 move /Y "*.nupkg" "..\..\InstallPackages"
-"..\..\..\oqtane.framework\oqtane.package\nuget.exe" pack ToSic.Imageflow.Oqtane.linux.nuspec
+.nuget\nuget.exe  pack ToSic.Imageflow.Oqtane.linux.nuspec
 ren ToSic.Imageflow.Oqtane.????.????.????.nupkg ToSic.Imageflow.Oqtane.????.????.????-linux.nupkg
 move /Y "*.nupkg" "..\..\InstallPackages"
-"..\..\..\oqtane.framework\oqtane.package\nuget.exe" pack ToSic.Imageflow.Oqtane.osx.nuspec
+.nuget\nuget.exe  pack ToSic.Imageflow.Oqtane.osx.nuspec
 ren ToSic.Imageflow.Oqtane.????.????.????.nupkg ToSic.Imageflow.Oqtane.????.????.????-osx.nupkg
 move /Y "*.nupkg" "..\..\InstallPackages"
