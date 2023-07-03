f94517d85ea2cb638a99bed635cab4fe33b45d45
Tonći Vatavuk <tonci@sistemi.hr>
2023-07-03 14:31:34 +0200

    update dependencies Oqtane 4.0.0, Imageflow 0.8.1, net7.0

--- Src/Package/debug.cmd
+++ Src/Package/debug.cmd
@@ -1,27 +1,27 @@
 @REM By convention if you place the oqtane module root folder at the same level as the oqtane framework, then you will not have to modify any folder configuration.
-XCOPY "..\Server\bin\Debug\net6.0\ToSic.Imageflow.Oqt.Server.Oqtane.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
-XCOPY "..\Server\bin\Debug\net6.0\ToSic.Imageflow.Oqt.Server.Oqtane.pdb" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
+XCOPY "..\Server\bin\Debug\net7.0\ToSic.Imageflow.Oqt.Server.Oqtane.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
+XCOPY "..\Server\bin\Debug\net7.0\ToSic.Imageflow.Oqt.Server.Oqtane.pdb" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
 XCOPY "..\Server\wwwroot\Modules\ToSic.Imageflow.Oqtane\*" "..\..\..\oqtane.framework\Oqtane.Server\wwwroot\Modules\ToSic.Imageflow.Oqtane\" /Y /S /I
 @REM *** nuget dependences - imageflow, imazen
-@REM Imageflow.Server 0.7.7
-XCOPY "..\packages\imageflow.server\0.7.7\lib\netcoreapp3.1\Imageflow.Server.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
-@REM Imageflow.Server.HybridCache 0.7.7
-XCOPY "..\packages\imageflow.server.hybridcache\0.7.7\lib\netcoreapp3.1\Imageflow.Server.HybridCache.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
-@REM Imazen.Common (>= 0.7.7)
-XCOPY "..\packages\imazen.common\0.7.7\lib\netstandard2.0\Imazen.Common.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
-@REM Imazen.HybridCache (>= 0.7.7) 
-XCOPY "..\packages\imazen.hybridcache\0.7.7\lib\netstandard2.0\Imazen.HybridCache.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
-@REM Imageflow.AllPlatforms (>= 0.8.3) 
-XCOPY "..\packages\imageflow.allplatforms\0.8.3\lib\netstandard2.0\Imageflow.AllPlatforms.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
-@REM Imageflow.Net (>= 0.8.3)
-XCOPY "..\packages\imageflow.net\0.9.0\lib\netstandard2.0\Imageflow.Net.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
-@REM Imageflow.NativeRuntime.osx-x86_64 (>= 1.7.3-rc67)
-@REM Imageflow.NativeRuntime.ubuntu-x86_64 (>= 1.7.3-rc67)
-@REM Imageflow.NativeRuntime.win-x86 (>= 1.7.3-rc67)
-@REM Imageflow.NativeRuntime.win-x86_64 (>= 1.7.3-rc67) 
-XCOPY "..\packages\imageflow.nativeruntime.win-x86_64\1.7.3-rc67\runtimes\win-x64\native\imageflow.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\runtimes\win-x64\native\" /Y
+@REM Imageflow.Server 0.8.1
+XCOPY "..\packages\imageflow.server\0.8.1\lib\net6.0\Imageflow.Server.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
+@REM Imageflow.Server.HybridCache 0.8.1
+XCOPY "..\packages\imageflow.server.hybridcache\0.8.1\lib\net6.0\Imageflow.Server.HybridCache.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
+@REM Imazen.Common (>= 0.8.1)
+XCOPY "..\packages\imazen.common\0.8.1\lib\netstandard2.0\Imazen.Common.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
+@REM Imazen.HybridCache (>= 0.8.1) 
+XCOPY "..\packages\imazen.hybridcache\0.8.1\lib\netstandard2.0\Imazen.HybridCache.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
+@REM Imageflow.AllPlatforms (>= 0.10.0) 
+XCOPY "..\packages\imageflow.allplatforms\0.10.0\lib\netstandard2.0\Imageflow.AllPlatforms.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
+@REM Imageflow.Net (>= 0.10.0)
+XCOPY "..\packages\imageflow.net\0.10.0\lib\netstandard2.0\Imageflow.Net.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
+@REM Imageflow.NativeRuntime.osx-x86_64 (>= 2.0.0-preview5)
+@REM Imageflow.NativeRuntime.ubuntu-x86_64 >= 2.0.0-preview5)
+@REM Imageflow.NativeRuntime.win-x86 (>= 2.0.0-preview5)
+@REM Imageflow.NativeRuntime.win-x86_64 >= 2.0.0-preview5) 
+XCOPY "..\packages\imageflow.nativeruntime.win-x86_64\2.0.0-preview5\runtimes\win-x64\native\imageflow.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\runtimes\win-x64\native\" /Y
 @REM Microsoft.IO.RecyclableMemoryStream (>= 1.2.2 && < 3.0.0) 
-XCOPY "..\packages\microsoft.io.recyclablememorystream\1.2.2\lib\netstandard1.4\Microsoft.IO.RecyclableMemoryStream.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
+XCOPY "..\packages\microsoft.io.recyclablememorystream\1.2.2\lib\netstandard1.4\Microsoft.IO.RecyclableMemoryStream.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
 @REM *** nuget dependences that are already in Oqtane 3.2.0
 @REM Microsoft.CSharp (>= 4.7.0)
 @REM Microsoft.Extensions.Hosting.Abstractions (>= 2.2.0)
@@ -36,4 +36,5 @@ XCOPY "..\packages\microsoft.io.recyclablememorystream\1.2.2\lib\netstandard1.4\
 @REM System.Runtime.CompilerServices.Unsafe (>= 6.0.0)
 @REM System.Threading.Tasks.Extensions (>= 4.5.4)
 @REM System.ValueTuple (>= 4.5.0)
-@REM Newtonsoft.Json (>= 10.0.3 && < 14.0.0)
+@REM Newtonsoft.Json (>= 13.0.3 && < 14.0.0)
+XCOPY "..\packages\newtonsoft.json\13.0.3\lib\net6.0\Newtonsoft.Json.dll" "..\..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
--- Src/Package/release.cmd
+++ Src/Package/release.cmd
@@ -1,6 +1,6 @@
 del "*.nupkg"
-del /F/Q/S "..\Server\bin\Release\net6.0\publish" > NUL
-rmdir /Q/S "..\Server\bin\Release\net6.0\publish"
+del /F/Q/S "..\Server\bin\Release\net7.0\publish" > NUL
+rmdir /Q/S "..\Server\bin\Release\net7.0\publish"
 dotnet publish ..\Server\ToSic.Imageflow.Oqtane.Server.csproj /p:Configuration=Release
 @REM By convention if you place the oqtane module root folder at the same level as the oqtane framework, then you will not have to modify any folder configuration.
 .nuget\nuget.exe pack ToSic.Imageflow.Oqtane.nuspec
